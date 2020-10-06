770558ad5a058ed7a586b05f8361c9cc66a00f30
dj-lei <m18349125880@gmail.com>
2020-10-06 08:43:36 +0800

    ADD doc handle

--- kg_extract/doc_handle.py
+++ kg_extract/doc_handle.py
@@ -2,6 +2,7 @@ from docx import Document
 from abc import abstractmethod
 import pandas as pd
 import re
+import zipfile
 
 
 class BaseDocHandle(object):
@@ -10,8 +11,8 @@ class BaseDocHandle(object):
         self.word = ''
         self.proper_nouns = set()
         self.document = Document(path)
-        with open('kg_extract/google-10000-english-usa-no-swears.txt', 'r') as f:
-            self.common_words = [token.strip() for token in f]
+        # with open('kg_extract/google-10000-english-usa-no-swears.txt', 'r') as f:
+        #     self.common_words = [token.strip() for token in f]
 
     def get_proper_nouns_csv(self):
         self.proper_nouns = set(pd.read_csv('proper_nouns.csv')['proper_nouns_name'].values)
@@ -65,8 +66,36 @@ class BaseDocHandle(object):
 
         return data
 
+    def get_doc_images(self, docxpath, dstpath):
+        doc = zipfile.ZipFile(docxpath)
+        for info in doc.infolist():
+            if info.filename.endswith((".png", ".jpeg", ".jpg", ".gif", ".emf")):
+                doc.extract(info.filename, dstpath)
+        doc.close()
+
+    def get_doc_tables(self):
+        table = self.document.tables[2]
+
+        ls = []
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    ls.append(paragraph.text)
+
+        row = 0
+        column = 0
+        df = pd.DataFrame(np.array(ls).reshape(row, column))  # reshape to the table shape
+        new = docx.Document()
+        word_table = new.add_table(rows=row, cols=column)
+        for x in range(0, row, 1):
+            for y in range(0, column, 1):
+                cell = word_table.cell(x, y)
+                cell.text = df.iloc[x, y]
+
+        return new, df
+
     @abstractmethod
-    def cut_special_symbols(self, word):
+    def cut_special_symbols_and_filter_rules(self, word):
         pass
 
     @abstractmethod
@@ -79,9 +108,16 @@ class RuDocHandle(BaseDocHandle):
     def __init__(self, path):
         super(RuDocHandle, self).__init__(path)
 
-    def cut_special_symbols(self, word):
-        r = "[\s\.\!\$%^*\"\'\:\;\,\?\(\)\“\‘\、]"
-        self.word = re.sub(r, '', word)
+    def cut_special_symbols_and_filter_rules(self, word):
+        r_symbols = "[\s\.\!\$%^*\"\'\:\;\,\?\(\)\[\]\“\”\‘\’\、]"
+        r_symbols_special = '[\-\/]$'
+        r_filter = '[0-9A-Z]-[0-9A-Z]'
+        # cut_special_symbols
+        self.word = re.sub(r_symbols, '', word).strip()
+        self.word = re.sub(r_symbols_special, '', self.word)
+        # filter_rules
+        if len(re.findall(r_filter, self.word)) > 0:
+            return ''
         return self.word
 
     def get_proper_nouns(self):
@@ -92,21 +128,17 @@ class RuDocHandle(BaseDocHandle):
             doc = self.document.paragraphs[i].text.strip()
             if doc not in ['', '\n']:
                 for word in doc.split(' '):
-                    if len(word) <= 1:
-                        continue
                     if word.isupper():
-                        proper_nouns_name = self.cut_special_symbols(word)
-                    elif len(re.findall('[(](.*?)[)]', word)):
-                        proper_nouns_name = re.findall('[(](.*?)[)]', word)[0]
+                        proper_nouns_name = word
+                    elif len(re.findall('[\(](.*?)[\)]', word)) > 0:
+                        proper_nouns_name = re.findall('[\(](.*?)[\)]', word)[0]
                     else:
                         continue
 
+                    proper_nouns_name = self.cut_special_symbols_and_filter_rules(proper_nouns_name)
                     if len(proper_nouns_name) <= 1:
                         continue
-                    # elif self.cut_special_symbols(word.lower()) not in self.common_words:
-                    #     proper_nouns_name = self.cut_special_symbols(word)
-
-                    data.loc[count, 'proper_nouns_name'] = proper_nouns_name.replace(' ','')
+                    data.loc[count, 'proper_nouns_name'] = proper_nouns_name
                     data.loc[count, 'text'] = doc
                     count = count + 1
         data['file_name'] = self.document.core_properties.title
