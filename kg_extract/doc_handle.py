355eb519159f53cccf33ef232fc0c0327beda3a2
dj-lei <m18349125880@gmail.com>
2020-10-14 18:13:24 +0800

    ADD new files

--- kg_extract/doc_handle.py
+++ kg_extract/doc_handle.py
@@ -1,44 +1,52 @@
 from docx import Document
 from abc import abstractmethod
 import pandas as pd
+import numpy as np
 import re
 import zipfile
+
+
+def Doctable(ls, row, column):
+    df = pd.DataFrame(np.array(ls).reshape(row, column))  # reshape to the table shape
+    df.columns = df.loc[0, :].values
+    df = df.loc[1:, :].dropna(how="all").drop_duplicates().reset_index(drop=True)
+    return df
 
 
 class BaseDocHandle(object):
 
     def __init__(self, path):
-        self.word = ''
+        self.path = path
         self.proper_nouns = set()
-        self.document = Document(path)
+        self.document = Document(self.path)
         # with open('kg_extract/google-10000-english-usa-no-swears.txt', 'r') as f:
         #     self.common_words = [token.strip() for token in f]
 
     def get_proper_nouns_csv(self):
         self.proper_nouns = set(pd.read_csv('proper_nouns.csv')['proper_nouns_name'].values)
 
-    def get_doc_structure(self):
+    def get_docx_structure(self):
         count = 0
         doc_name = self.document.core_properties.title
-        self.get_proper_nouns_csv()
+        # self.get_proper_nouns_csv()
 
         level = 0
         content = [0 for _ in range(0, 10)]
         content[0] = doc_name
         temp_name = ''
-        data = pd.DataFrame([], columns=['s', 'p', 'o', 'key_words'])
+        data = pd.DataFrame([], columns=['s', 'p', 'o'])
         for i in range(0, len(self.document.paragraphs)):
             doc = self.document.paragraphs[i].text.strip()
             style_name = self.document.paragraphs[i].style.name
             if doc in ['', '\n']:
                 continue
 
-            key_words = str(list(self.proper_nouns.intersection(set(doc.split(' ')))))
+            # key_words = str(list(self.proper_nouns.intersection(set(doc.split(' ')))))
             if style_name == 'Heading':
                 data.loc[count, 's'] = content[0]
                 data.loc[count, 'p'] = 'Heading 0'
                 data.loc[count, 'o'] = doc
-                data.loc[count, 'key_words'] = key_words
+                # data.loc[count, 'key_words'] = key_words
                 temp_name = doc
             elif 'Heading' in style_name:
                 level = int(style_name.split(' ')[1])
@@ -47,52 +55,56 @@ class BaseDocHandle(object):
                 data.loc[count, 's'] = content[level-1]
                 data.loc[count, 'p'] = style_name
                 data.loc[count, 'o'] = doc
-                data.loc[count, 'key_words'] = key_words
+                # data.loc[count, 'key_words'] = key_words
             else:
                 if level == 0:
                     data.loc[count, 's'] = temp_name
                     data.loc[count, 'p'] = 'Heading ' + str(level + 1)
                     data.loc[count, 'o'] = doc
-                    data.loc[count, 'key_words'] = key_words
+                    # data.loc[count, 'key_words'] = key_words
                     count = count + 1
                     continue
+
+                if style_name in ['Caption', 'CaptionFigure']:
+                    data.loc[count, 's'] = content[level]
+                    data.loc[count, 'p'] = 'Caption'
+                    data.loc[count, 'o'] = doc
+                    # data.loc[count, 'key_words'] = key_words
+                    count = count + 1
+                    continue
+
                 data.loc[count, 's'] = content[level]
                 data.loc[count, 'p'] = 'Heading '+str(level+1)
                 data.loc[count, 'o'] = doc
-                data.loc[count, 'key_words'] = key_words
+                # data.loc[count, 'key_words'] = key_words
             count = count + 1
 
         data = data.reset_index(drop=True)
-
         return data
 
-    def get_doc_images(self, docxpath, dstpath):
-        doc = zipfile.ZipFile(docxpath)
+    def get_docx_images(self, dstpath):
+        doc = zipfile.ZipFile(self.path)
         for info in doc.infolist():
-            if info.filename.endswith((".png", ".jpeg", ".jpg", ".gif", ".emf")):
-                doc.extract(info.filename, dstpath)
+            if info.filename.endswith((".png", ".jpeg", ".jpg", ".emf")):
+                if int(info.file_size) > 2000:
+                    doc.extract(info.filename, dstpath)
         doc.close()
 
-    def get_doc_tables(self):
-        table = self.document.tables[2]
+    def get_docx_tables(self):
+        result = []
+        for table in self.document.tables:
+            ls = []
+            if len(table.rows) <= 1:
+                continue
+            for row in table.rows:
+                for cell in row.cells:
+                    temp = []
+                    for paragraph in cell.paragraphs:
+                        temp.append(paragraph.text)
+                    ls.append('\n'.join(temp))
 
-        ls = []
-        for row in table.rows:
-            for cell in row.cells:
-                for paragraph in cell.paragraphs:
-                    ls.append(paragraph.text)
-
-        row = 0
-        column = 0
-        df = pd.DataFrame(np.array(ls).reshape(row, column))  # reshape to the table shape
-        new = docx.Document()
-        word_table = new.add_table(rows=row, cols=column)
-        for x in range(0, row, 1):
-            for y in range(0, column, 1):
-                cell = word_table.cell(x, y)
-                cell.text = df.iloc[x, y]
-
-        return new, df
+            result.append(Doctable(ls, len(table.rows), len(table.rows[0].cells)))
+        return result
 
     @abstractmethod
     def cut_special_symbols_and_filter_rules(self, word):
