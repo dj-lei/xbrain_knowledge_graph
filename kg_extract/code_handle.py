dabb77e55241f7ba2b33bc54a734aebde73effc1
dj-lei <m18349125880@gmail.com>
2020-10-16 18:27:36 +0800

    ADD new functions

---
+++ kg_extract/code_handle.py
@@ -0,0 +1,11 @@
+class BaseCodeHandle(object):
+    def __init__(self):
+        pass
+
+
+class PythonCodeHandle(BaseCodeHandle):
+    def __init__(self):
+        super(PythonCodeHandle, self).__init__()
+
+    def get_file_architecture(self):
+        pass
